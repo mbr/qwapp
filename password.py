628585c9ae2854780af11409dac8e8686238458b
Marc Brinkmann <git@marcbrinkmann.de>
2011-03-11 15:15:06 +0100

    Password module.

---
+++ password.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding=utf8
+
+import os
+from hashlib import sha256
+
+def hash_password(password, salt = None, iterations = 1000):
+	if not salt: salt = sha256(os.urandom(256)).hexdigest()
+
+	h = password
+	for i in xrange(0,iterations):
+		h = sha256(salt + h).hexdigest()
+	return salt + h
+
+
+def check_password(password, pwdata):
+	digest_len = 64
+	# sha256 digestsize is 32 bytes
+	salt = pwdata[:digest_len]
+	pwhash = pwdata[digest_len:]
+	return hash_password(password, salt) == pwdata
