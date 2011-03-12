a467b0b27693b8a319ae833421087683099c32f9
Marc Brinkmann <git@marcbrinkmann.de>
2011-03-12 20:01:23 +0100

    Support passing configuration file as WSGI argument.

--- qwapp/__init__.py
+++ qwapp/__init__.py
@@ -5,16 +5,18 @@ from flask import Flask, g, url_for
 from flaskext.markdown import Markdown
 from flaskext.cache import Cache
 from qwapp.views.frontend import frontend
-import defconfig
+
+import defaults
 
 from db import WikiDb
 
-def create_app():
+def create_app(configuration_file = None):
 	app = Flask(__name__)
 
-	# load a default config, and from envvar
-	app.config.from_object(defconfig)
-	#app.config.from_envvar('QWAPP_CONFIG')
+	# load a default config, and from configuration file
+	app.config.from_object(defaults)
+	if configuration_file:
+		app.config.from_file(configuration_file)
 
 	app.db = WikiDb(app.config['REPOSITORY_PATH'])
 	app.md = Markdown(app, safe_mode = False, extensions = ['wikilinks'], extension_configs = { 'wikilinks': [('build_url', lambda name, base, end: url_for('show_page', name = name))] })
