23a5dc818de52a14c89b8789852cdd9f4d19d9b5
Marc Brinkmann <git@marcbrinkmann.de>
2011-03-12 20:54:06 +0100

    runserver handles commandline arguments for supply config files.

--- runserver.py
+++ runserver.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 # coding=utf8
 
+import sys
+
 from qwapp import create_app
 
-app = create_app()
+if len(sys.argv) == 2:
+	print 'using configuration file',sys.argv[1]
+	app = create_app(sys.argv[1])
+else:
+	app = create_app()
+
 app.run(use_debugger = True, use_reloader = True)
