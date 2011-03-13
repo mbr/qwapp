5874837389dcf86389f04aba36b5921800522925
Marc Brinkmann <git@marcbrinkmann.de>
2011-03-13 13:38:43 +0100

    Use headershift extension in Wiki.

--- qwapp/__init__.py
+++ qwapp/__init__.py
@@ -19,7 +19,7 @@ def create_app(configuration_file = None):
 		app.config.from_pyfile(configuration_file)
 
 	app.db = WikiDb(app.config['REPOSITORY_PATH'])
-	app.md = Markdown(app, safe_mode = False, extensions = ['wikilinks2'], extension_configs = { 'wikilinks2': [('build_href', lambda target, label: url_for('show_page', name = target))] })
+	app.md = Markdown(app, safe_mode = False, extensions = ['wikilinks2', 'headershift'], extension_configs = { 'wikilinks2': [('build_href', lambda target, label: url_for('show_page', name = target))], 'headershift': [('shift_amount', 1)] })
 	app.cache = Cache(app)
 
 	app.register_module(frontend)
