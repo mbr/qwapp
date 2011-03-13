2f0f2bd3460a185bad6c6c48cd546fd4945f6ac9
Marc Brinkmann <git@marcbrinkmann.de>
2011-03-13 13:34:28 +0100

    Fixed wrong use of config in mdx_wikilinks2.

--- mdx_wikilinks2.py
+++ mdx_wikilinks2.py
@@ -8,9 +8,9 @@ class WikiLinks2Pattern(markdown.inlinepatterns.Pattern):
 		self.ext = ext
 		markdown.inlinepatterns.Pattern.__init__(self, ext.WIKI_RE, md)
 
-		self.build_link = ext.config['build_link']
-		self.build_href = ext.config['build_href']
-		self.link_class = ext.config['link_class']
+		self.build_link = ext.getConfig('build_link')
+		self.build_href = ext.getConfig('build_href')
+		self.link_class = ext.getConfig('link_class')
 
 	def handleMatch(self, m):
 		label = (m.group(3) or m.group(2)) + m.group(4)
@@ -38,10 +38,10 @@ class Wikilinks2Extension(markdown.Extension):
 
 	def __init__(self, configs):
 		self.config = {
-			'build_href': build_href,
-			'build_link': build_link,
-			'link_exp': self.WIKI_RE,
-			'link_class': 'wikilink',
+			'build_href': [build_href, 'A callback that given target and label, constructs a URL'],
+			'build_link': [build_link, 'A callback that constructs a link element using ElementTree from the label, href, title and class attributes.'],
+			'link_exp': [self.WIKI_RE, 'Regular expression used to find Wiki-links.'],
+			'link_class': ['wikilink', 'The class applied to all links.'],
 		}
 
 		for key, value in configs:
