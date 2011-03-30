0ad0fd4c5695f7544312b12e43ba816da7f5d5ec
Marc Brinkmann <git@marcbrinkmann.de>
2011-03-30 14:38:33 +0200

    Added abspath to runserver script.

--- runserver.py
+++ runserver.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # coding=utf8
 
+import os
 import sys
 
 from qwapp import create_app
 
 if len(sys.argv) == 2:
 	print 'using configuration file',sys.argv[1]
-	app = create_app(sys.argv[1])
+	app = create_app(os.path.abspath(sys.argv[1]))
 else:
 	app = create_app()
 
